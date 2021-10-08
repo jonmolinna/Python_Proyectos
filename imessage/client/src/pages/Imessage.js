c7801760969012969b16351d9286d9e78b24bac8
jonmolinna <jmolina2624@gmail.com>
2021-10-08 15:58:21 -0500

    Pages

--- imessage/client/src/pages/Imessage.js
+++ imessage/client/src/pages/Imessage.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import Chat from '../componentes/Chat';
 import Sidebar from '../componentes/Sidebar';
+import Home from '../componentes/Home';
 import './Imessage.css';
 
+import { useAuthState } from '../context/auth';
+
 const Imessage = () => {
+    const { messageID } = useAuthState();
+
     return (
         <div className="imessage">
-            <div className="imessage__sidebar">
-                <Sidebar />
+            <div className="imessage__movil">
+                {
+                    messageID? <Chat /> : <Sidebar />
+                }
             </div>
-            <div className="imessage__chat">
-                <Chat />
+
+            <div className="imessage__desktop">
+                <div className="imessage__sidebar">
+                    <Sidebar />
+                </div>
+                <div className="imessage__chat">
+                    {
+                        messageID? <Chat /> : <Home />
+                    }
+                    
+                </div>
             </div>
-            
         </div>
     )
 }
--- imessage/client/src/pages/Login.js
+++ imessage/client/src/pages/Login.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import { toast } from 'react-toastify';
+
+import { useAuthDispatch } from '../context/auth';
 
 const Login = () => {
+    const dispatch = useAuthDispatch();
+    const [username, setUsername] = useState('');
+
+    const sendUsername = (e) => {
+        e.preventDefault();
+        if(!username) return toast.error('Ingrese su Nombre');
+        if(username.trim() === "") return toast.warning('La casilla esta en Blanco')
+
+        dispatch({
+            type: 'LOGIN',
+            payload: username.trim()
+        });
+        setUsername('');
+    }
+
     return (
         <div className="login">
             <img 
@@ -9,8 +27,13 @@ const Login = () => {
                 alt="logo__telegram" 
             />
             <h1>Telegram Web</h1>
-            <form className="login__form">
-                <input type="text" placeholder="Ingrese Nombre" />
+            <form className="login__form" onSubmit={sendUsername}>
+                <input 
+                    type="text" 
+                    placeholder="Ingrese Nombre" 
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button>Ingresar</button>
             </form>
         </div>
